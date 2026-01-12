cabe27ef1572e9bd0702bf6bf7e74f9b7bb0a01d
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2026-01-12 23:00:58 +0530

    extract input field values

--- tests/extractingValues.spec.ts
+++ tests/extractingValues.spec.ts
@@ -8,7 +8,7 @@ test.beforeEach(async ({page}) => {
 })
 
 // ******************** Test cases *******************
-test('extract single test value', async ({page}) => {
+test('extract single text value', async ({page}) => {
     const BasicFormcard = page.locator('nb-card').filter({hasText: 'Basic form'})
     const BasicFormCardButtonContent = await BasicFormcard.getByRole('button').textContent()
 
@@ -16,7 +16,7 @@ test('extract single test value', async ({page}) => {
     expect(BasicFormCardButtonContent).toEqual('Submit')
 })
 
-test('extract multiple values', async ({page}) => {
+test('extract multiple text values', async ({page}) => {
     const UsingTheGridCard = page.locator('nb-card').filter({hasText: 'Using the Grid'})
     const AllRadioButtonContent = await UsingTheGridCard.locator('nb-radio').allTextContents()
 
@@ -27,6 +27,21 @@ test('extract multiple values', async ({page}) => {
     expect(AllRadioButtonContent).toContain('Disabled Option')
     expect(AllRadioButtonContent.length).toBe(3)
     expect(AllRadioButtonContent).not.toContainEqual('Option 1,Option 2,Disabled Option')
+})
+
+test('extract input field values', async ({page}) => {
+    const BasicFormcard = page.locator('nb-card').filter({hasText: 'Basic form'})
+    const EmailInput = BasicFormcard.getByPlaceholder('Email')
+    const PasswordInput = BasicFormcard.getByPlaceholder('Password')
+
+    await EmailInput.fill('Raviprasad@gmail.com')
+    await PasswordInput.fill('Raviprasad@123')
+
+    const EmailInputValue = await EmailInput.inputValue()
+    const PasswordInputValue = await PasswordInput.inputValue()
+
+    expect(EmailInput).toHaveValue('Raviprasad@gmail.com')
+    expect(PasswordInput).toHaveValue('Raviprasad@123')
 })
 
 // **************** Teardown hooks *******************
