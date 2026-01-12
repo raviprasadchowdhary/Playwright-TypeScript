9390d9a71f702624d42a3f2cfb0cd174f1223f8b
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2026-01-12 22:48:35 +0530

    extract multiple text values

--- tests/extractingValues.spec.ts
+++ tests/extractingValues.spec.ts
@@ -8,12 +8,25 @@ test.beforeEach(async ({page}) => {
 })
 
 // ******************** Test cases *******************
-test('single test value', async ({page}) => {
+test('extract single test value', async ({page}) => {
     const BasicFormcard = page.locator('nb-card').filter({hasText: 'Basic form'})
     const BasicFormCardButtonContent = await BasicFormcard.getByRole('button').textContent()
 
     // console.log(`BasicFormCardButtonContent: ${BasicFormCardButtonContent}`)
     expect(BasicFormCardButtonContent).toEqual('Submit')
+})
+
+test('extract multiple values', async ({page}) => {
+    const UsingTheGridCard = page.locator('nb-card').filter({hasText: 'Using the Grid'})
+    const AllRadioButtonContent = await UsingTheGridCard.locator('nb-radio').allTextContents()
+
+    console.log(`UsingTheGridCardRadioGroupContent: ${AllRadioButtonContent}`)
+
+    expect(AllRadioButtonContent).toContain('Option 1')
+    expect(AllRadioButtonContent).toContain('Option 2')
+    expect(AllRadioButtonContent).toContain('Disabled Option')
+    expect(AllRadioButtonContent.length).toBe(3)
+    expect(AllRadioButtonContent).not.toContainEqual('Option 1,Option 2,Disabled Option')
 })
 
 // **************** Teardown hooks *******************
