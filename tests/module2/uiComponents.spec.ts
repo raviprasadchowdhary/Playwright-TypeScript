26f1ed4d1ed013bb6cd90c663617b68fbaac449a
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2026-01-15 22:31:00 +0530

    input field

--- tests/module2/uiComponents.spec.ts
+++ tests/module2/uiComponents.spec.ts
@@ -1,8 +1,10 @@
-import {test} from '@playwright/test'
+import {expect, test} from '@playwright/test'
 
 // ******************* Setup hooks *******************
 test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
 })
 
 // ****************** Teardown hooks *******************
@@ -12,8 +14,17 @@ test.afterEach(async ({page}) => {
 
 // ******************** Test cases *******************
 test.describe('Form Layouts page', () => {
-    // Test case for basic form layout
-    test('', async ({page}) => {
-    // Test case implementation goes here
-    })
+    test('input field', async ({page}) => {
+        const EmailInput = 'raviprasad@gmail.com'
+        const EmailUsingTheGrid = page.locator('nb-card').filter({hasText: 'Using the Grid'}).getByRole('textbox', {name: 'Email'})
+        
+        await EmailUsingTheGrid.fill(EmailInput)    // filling input field
+        expect(await EmailUsingTheGrid.inputValue()).toEqual(EmailInput)  // general assertion
+
+        await EmailUsingTheGrid.clear() // clearing input field
+        expect(await EmailUsingTheGrid.inputValue()).toEqual('')  // general assertion
+        
+        await EmailUsingTheGrid.pressSequentially(EmailInput, {delay: 50})  // typing with delay
+        await expect(EmailUsingTheGrid).toHaveValue(EmailInput)    // locator assertion
+        })
 })
