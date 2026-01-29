7833ef061498992c5fccc622054ef1c1f4e435d1
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2026-01-29 21:54:22 +0530

    click on datepicker and select a date

---
+++ tests/module3/datepicker.spec.ts
@@ -0,0 +1,26 @@
+import {expect, test} from 'playwright/test';
+
+// ******************* Setup hooks *******************
+test.beforeEach(async({page}) => {
+    await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
+    await page.getByText('Datepicker').click()
+})
+
+// ****************** Teardown hooks *******************
+
+// ******************** Test cases *******************
+test.describe('Datepicker tests', () => {
+    test('click on datepicker and select a date', async({page}) => {
+        const DatepickerInput = page.getByPlaceholder('Form Picker')
+        await DatepickerInput.click()
+
+        const Activeday = page.locator('[class="day-cell ng-star-inserted"]')
+        await Activeday.getByText('1', {exact: true}).click()
+
+        const CurrentDate = new Date()
+        const CurrentYear = CurrentDate.getFullYear()
+        const CurrentMonth = CurrentDate.toLocaleString('default', {month: 'short'})
+        expect(DatepickerInput).toHaveValue('' + CurrentMonth + ' 1, ' + CurrentYear)
+    })
+})
