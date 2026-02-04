50d8e94998f1bf47c9be683a55b005bb0c505ace
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2026-02-04 22:19:31 +0530

    helper method

--- pageObjects/NavigationPage.ts
+++ pageObjects/NavigationPage.ts
@@ -8,22 +8,44 @@ export class NavigationPage {
     }
 
     async formLayoutsPage(){
-        await this.page.getByText('Forms').click()
+        await this.menuItemClick('Forms')
+        console.log('Navigating to Form Layouts page')
         await this.page.getByText('Form Layouts').click()
+        console.log('Navigated to Form Layouts page')
     }
 
     async datePickerPage(){
-        await this.page.getByText('Forms').click()
+        await this.menuItemClick('Forms')
+        console.log('Navigating to Datepicker page')
         await this.page.getByText('Datepicker').click()
+        console.log('Navigated to Datepicker page')
     }
 
     async smartTablePage(){
-        await this.page.getByText('Tables & Data').click()
+        await this.menuItemClick('Tables & Data')
+        console.log('Navigating to Smart Table page')
         await this.page.getByText('Smart Table').click()
+        console.log('Navigated to Smart Table page')
     }
 
     async tostrPage(){
-        await this.page.getByText('Modal & Overlays').click()
+        await this.menuItemClick('Modal & Overlays')
+        console.log('Navigating to Toastr page')
         await this.page.getByText('Toastr').click()
+        console.log('Navigated to Toastr page')
+    }
+
+    private async menuItemClick(menuItem: string){
+        const menuItemElement = this.page.getByTitle(menuItem)
+        console.log(`menuItem: ${menuItem}`)
+        const currentState = await menuItemElement.getAttribute('aria-expanded') 
+        console.log(`currentState: ${currentState}`)
+
+        if(currentState == 'false'){
+            await menuItemElement.click()
+            console.log(`${menuItem} menu expanded`)
+        }else{
+            console.log(`${menuItem} menu already expanded`)
+        }
     }
 }
