49b5af072de913f06b68a5cd0e365161f3fe3852
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2026-02-04 21:43:14 +0530

    NavigationPage

--- pageObjects/NavigationPage.ts
+++ pageObjects/NavigationPage.ts
@@ -11,4 +11,19 @@ export class NavigationPage {
         await this.page.getByText('Forms').click()
         await this.page.getByText('Form Layouts').click()
     }
+
+    async datePickerPage(){
+        await this.page.getByText('Forms').click()
+        await this.page.getByText('Datepicker').click()
+    }
+
+    async smartTablePage(){
+        await this.page.getByText('Tables & Data').click()
+        await this.page.getByText('Smart Table').click()
+    }
+
+    async tostrPage(){
+        await this.page.getByText('Modal & Overlays').click()
+        await this.page.getByText('Toastr').click()
+    }
 }
